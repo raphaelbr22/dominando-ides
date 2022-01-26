9e2111b6ea170d7536a3bf5511026f503ab8d8c7
raphaelbr22 <raphael.fa.el@hotmail.com>
2022-01-25 23:20:32 -0300

    First commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,8 +6,9 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		
-		Gato gato = new Gato();
 		Livros Livros = new Livros();
+		Gato gato = new Gato();
+		
 		
 		System.out.println(gato);
 		System.out.println(Livros);
